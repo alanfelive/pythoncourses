1b67f60b8ff88ee4d8916d6050e60fa614f1c234
Федотов А.А <al_fed@live.ru>
2020-11-12 06:17:27 +0400

    task 1

--- lesson_1/task_1.py
+++ lesson_1/task_1.py
@@ -2,3 +2,9 @@ firstName = "Александр"
 lastName = "Федотов"
 age = 32
 print(firstName + " " + lastName + ", " + str(age) + " года")
+
+firstName = input("Ваше имя?")
+lastName = input("Ваша фамилия?")
+age = input("Ваш возраст?")
+
+print("Вас зовут " + firstName + " " + lastName + ", ваш возраст " + age + " лет (года)")

7749f93e03552a57652ed123bc5609b937c4189a
Федотов А.А <al_fed@live.ru>
2020-11-12 07:58:44 +0400

    task 6

---
+++ lesson_1/task_6.py
@@ -0,0 +1,9 @@
+a = int(input("Результаты пробежки в первый день?"))
+b = int(input("Желаемый результат?"))
+days = 1
+result = a
+while result < b:
+        a = a + 0.1 * a
+        days += 1
+        result = result + a
+print(f"Спортсмен достигнет требуемых результатов на %.d день" % days)

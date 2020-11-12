ed890122d7df525f41804d86cfabf904e5405fa1
Федотов А.А <al_fed@live.ru>
2020-11-12 06:48:13 +0400

    task 2

---
+++ lesson_1/task_2.py
@@ -0,0 +1,6 @@
+seconds = int(input("введите время в секундах"))
+
+timeH = seconds // 3600
+timeM = (seconds - timeH * 3600) // 60
+timeS = seconds - (timeH * 3600 + timeM * 60)
+print("Точное время " + f"{timeH:02d}" + ":" + f"{timeM:02d}" + ":" + f"{timeS:02d}")
